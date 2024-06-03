da420d62d299cab4fda3fe4e05b10530e538a3ea
maalberg <andrei.maalberg@gmail.com>
2024-06-03 09:39:53 +0200

    clean utilities

--- utilities.py
+++ utilities.py
@@ -1,34 +1,15 @@
 import numpy as np
 import scipy as sp
-from functools import wraps
-
-def stack_args(first: int = 0):
-    """
-    Stack the arguments of a function starting from ``first`` argument.
-    The arguments are stacked horizontally, i.e. as columns
-    of an nd-array. Note that this method filters out
-    none arguments; this behavior is experimental.
-    """
-    def wrap(func):
-        @wraps(func)
-        def wrapped_func(*args, **kwargs):
-            # filter out none arguments and make sure the arguments are formatted as 2d arrays
-            stack = tuple(filter(lambda arg: True if arg is not None else False, args[first:]))
-            stack = tuple(map(np.atleast_2d, stack))
-
-            args_n = len(stack)
-            if args_n == 1:
-                args_stacked = stack
-            else:
-                args_stacked = (np.column_stack(stack),)
-
-            # update the list of arguments
-            args = args[:first] + args_stacked
-            return func(*args, **kwargs)
-        return wrapped_func
-    return wrap
 
 def make_dlqr(a: np.ndarray, b: np.ndarray, q: np.ndarray, r: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Synthesize a digital linear-quadratic regulator (lqr) using matrices ``a``, ``b``, ``q`` and ``r``,
+    where ``a`` and ``b`` are the system and input matrices of a plant to be regulated,
+    and where ``q`` and ``r`` are the state and control cost matrices, respectively.
+    The method returns a tuple with the regulator matrix K to be used as u = -K*x,
+    where x is the full state of the plant; and the solution to an algebraic
+    Riccati equation, which was used during the synthesis of K matrix.
+    """
     a, b, q, r = map(np.atleast_2d, (a, b, q, r))
     p = sp.linalg.solve_discrete_are(a, b, q, r)
 
@@ -42,6 +23,6 @@ def make_dlqr(a: np.ndarray, b: np.ndarray, q: np.ndarray, r: np.ndarray) -> tup
     bpb += r
     bpa = bp.dot(a)
     control = np.linalg.solve(bpb, bpa)
-    parameters = p
+    solution = p
 
-    return control, parameters
+    return control, solution
